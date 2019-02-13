b1bede5004b448f95916ad86d787bafa288e0e45
Steve <sjc.zambrano@yavirac.edu.ec>
2019-02-13 11:22:18 -0500

    Registro Ok

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,6 +1,7 @@
 import { Platform } from '@ionic/angular';
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 
 const TOKEN_KEY = 'auth-token';
@@ -12,7 +13,7 @@ export class AuthenticationService {
 
   authenticationState = new BehaviorSubject(false);
 
-  constructor(private storage: Storage, private plt: Platform) {
+  constructor(private storage: Storage, private plt: Platform, private http: HttpClient) {
     this.plt.ready().then(() => {
       this.checkToken();
     });
@@ -31,10 +32,18 @@ export class AuthenticationService {
       this.authenticationState.next(true);
     });
   }
-  register(name, email) {
-    console.log(name);
-    console.log(email);
-    console.log('Registro valido');
+  public register(name: String, email: String): Promise<any> {
+    const data = {name: name, email: email};
+    return new Promise((resolve, reject) => {
+      this.http.post('http://localhost:8000/register' , data).subscribe(
+        response => {
+          resolve(response);
+        },
+        exception => {
+          reject(exception);
+        }
+      );
+    });
   }
 
   logout() {
